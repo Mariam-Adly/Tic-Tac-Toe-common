332d1b791e8460c5a28f4529408df14205026366
Moustafa Essam <drsh_201122@yahoo.com>
2023-01-05 01:29:40 +0200

    add serialization example

--- src/TicTacToeCommon/TicTacToeCommon.java
+++ src/TicTacToeCommon/TicTacToeCommon.java
@@ -5,15 +5,51 @@
  */
 package TicTacToeCommon;
 
+import TicTacToeCommon.models.base.RemoteMessage;
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.io.ObjectInputStream;
+import java.io.ObjectOutputStream;
+import java.io.PrintStream;
+import java.io.Serializable;
+
 
 public class TicTacToeCommon {
 
     /**
      * @param args the command line arguments
      */
-    public static void main(String[] args) {
-        // TODO code application logic here
-        System.out.println("hello");
+    public static void main(String[] args) throws IOException, ClassNotFoundException {
+        deserialize();
+    }
+
+    private static void deserialize() throws IOException, ClassNotFoundException {
+        ObjectInputStream oin = new ObjectInputStream(new FileInputStream("remotemessage.bin"));
+        RemoteMessage message;
+        while ((message = (RemoteMessage) oin.readObject()) != null) {
+            if (message.getData() instanceof OurData) {
+                System.out.println("got data " + ((OurData)message.getData()).i);
+            }
+        }
+    }
+
+    private static void serialize() throws IOException {
+        RemoteMessage<OurData> message = new RemoteMessage<>(new OurData(1024));
+        ObjectOutputStream oout = new ObjectOutputStream(new PrintStream("remotemessage.bin"));
+        oout.writeObject(message);
+        oout.flush();
+        oout.close();
     }
     
+    static class OurData implements Serializable {
+        static final long serialVersionUID = 40L;
+        
+        private int i;
+        
+        public OurData() {}
+        
+        public OurData(int i) {
+            this.i = i;
+        } 
+    }
 }
