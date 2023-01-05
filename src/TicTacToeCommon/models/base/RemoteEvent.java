ac829a1d48a9753de88d7b68ae6c897408f080b8
Moustafa Essam <drsh_201122@yahoo.com>
2023-01-05 20:19:04 +0200

    add game events

---
+++ src/TicTacToeCommon/models/base/RemoteEvent.java
@@ -0,0 +1,13 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package TicTacToeCommon.models.base;
+
+/**
+ *
+ * @author m-essam
+ */
+public interface RemoteEvent extends RemoteSendable {
+    
+}
